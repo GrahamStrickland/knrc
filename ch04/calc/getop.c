ace3e54297ff1cc05e6068a51d5b255763d4bc63
GrahamStrickland <grahamhstrickland@gmail.com>
2023-12-30 11:37:22 +0200

    Add static internal variables to replace ungetch()

--- ch04/calc/getop.c
+++ ch04/calc/getop.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define BUFSIZE 100
+
 #include "calc.h"
 
 /* getop: get next operator or numeric operand */
 getop(char s[]) {
+    static char buf[BUFSIZE];
+    static int bufp = 0;
     int i, c;
 
-    while ((s[0] = c = getch()) == ' ' || c == '\t')
+    while ((bufp > 0 && (c = buf[bufp]) == ' ' || c == '\t') || 
+            ((s[0] = c = getch()) == ' ' || c == '\t'))
         ;
     s[1] = '\0';
     if (!isdigit(c) && c != '.')
@@ -20,8 +25,13 @@ getop(char s[]) {
         while (isdigit(s[++i] = c = getch()))
             ;
     s[i] = '\0';
-    if (c != EOF)
-        ungetch(c);
+    if (c != EOF) {
+        if (bufp >= BUFSIZE)
+            printf("getop: too many characters\n");
+        else
+            buf[bufp++] = c;
+    }
+
     return NUMBER;
 }
 
