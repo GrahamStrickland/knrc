a925ca3d4dfa7ed791ffe7ca1a37096d1124104e
Graham Strickland <grahamhstrickland@gmail.com>
2024-02-03 15:57:52 +0200

    Fix itoa.* with incorrect naming

--- src/ch04/recursiveitoa/itoa.c
+++ src/ch04/recursiveitoa/itoa.c
@@ -1,2 +1,16 @@
-void itoa(int n, char s[], int i);
+#include <stdio.h>
 
+#include "itoa.h"
+
+/* itoa: convert n to characters in s */
+void itoa(int n, char s[], int i)
+{
+    if (n < 0) {
+        s[0] = '-';
+        n = -n;
+    }
+    if (n / 10)
+        itoa(n / 10, s, i++);
+    s[i] = n % 10 + '0';
+    s[i+1] = '\0';
+}
--- src/ch04/recursiveitoa/itoa.h
+++ src/ch04/recursiveitoa/itoa.h
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-#include "itoa.h"
-
-/* itoa: convert n to characters in s */
-void itoa(int n, char s[], int i)
-{
-    if (n < 0) {
-        s[0] = '-';
-        n = -n;
-    }
-    if (n / 10)
-        itoa(n / 10, s, i++);
-    s[i] = n % 10 + '0';
-    s[i+1] = '\0';
-}
-
+void itoa(int n, char s[], int i);
