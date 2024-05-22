b60f6333431722e9f24aa45782eb30077851a58a
Graham Strickland <grahamhstrickland@gmail.com>
2024-05-22 20:38:07 +0200

    Add swap example

---
+++ src/pointers_arrays/swap.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *px, int *py);
+
+int main()
+{
+    int a = 2, b = 5;
+
+    printf("Before swap, a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After swap, a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
+void swap(int *px, int *py) /* interchange *px and *py */
+{
+    int temp;
+
+    temp = *px;
+    *px = *py;
+    *py = temp;
+}
+
